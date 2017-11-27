05da9645c6423af56822f9da6596d50b1dd54cf6
gorynychalex <gorynychalex@gmail.com>
2017-11-27 17:31:19 +1000

    Clear product prepared from MainActivity.

--- app/src/main/java/ru/popovich/shopclient/MainActivity.java
+++ app/src/main/java/ru/popovich/shopclient/MainActivity.java
@@ -50,47 +50,12 @@ public class MainActivity extends AppCompatActivity
 
     Intent intentBasket;
 
-    /// Models
-//    private Catalog catalogs = new Catalog();
-//    private List<ProdCategory> prodCategories1 = new ArrayList<>();
-//    private List<ProdCategory> prodCategories2 = new ArrayList<>();
-//    private List<ProdCategory> prodCategories3 = new ArrayList<>();
-//    private List<ModelProduct> products1 = new ArrayList<>();
-//    private List<ModelProduct> products2 = new ArrayList<>();
-//    private List<ModelProduct> products3 = new ArrayList<>();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         CatalogData.setCatalogs();
-
-        //////////////// CATALOG EXAMPLE //////////////////////////////
-//        catalogs.setName("Drinks and Snaks");
-        //Initialize Products
-//        products1.add(new ModelProduct("Sandwich", "Rio De Janeiro",R.drawable.sandwich,2F));
-//        products1.add(new ModelProduct("Hot dog", "Hot Evening", R.drawable.sandwich2, 2.5F));
-//        products1.add(new ModelProduct("Sanwich", "Reptile", R.drawable.sandwich, 5F));
-//
-//        prodCategories1.add(new ProdCategory("Sandwichs",products1));
-//
-//        products2.add(new ModelProduct("Apple Juice", "Apple Juice", R.drawable.shwepps,2F));
-//        products2.add(new ModelProduct("Orange Juice", "Orange Juice", R.drawable.ic_basket, 2.5F));
-//        products2.add(new ModelProduct("Strawberry Juice", "Strawberry Juice", R.drawable.shwepps, 5F));
-//
-//        prodCategories1.add(new ProdCategory("Drinks", products2));
-//
-//        products3.add(new ModelProduct("Noodle", "Noodle", R.drawable.shwepps,2.0F));
-//        products3.add(new ModelProduct("Chiken", "Chiken", R.drawable.sandwich, 2.5F));
-//        products3.add(new ModelProduct("Nagins", "Nagins", R.drawable.ic_basket, 5F));
-//
-//
-//        prodCategories1.add(new ProdCategory("Drinks", products3));
-//
-//        catalogs.setCategories(prodCategories1);
-
-
         ///////////////// BASKET INITIALIZE //////////////////////////////
         basket = new Basket();
 
@@ -227,13 +192,6 @@ public class MainActivity extends AppCompatActivity
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//            return super.onCreateView(inflater, container, savedInstanceState);
-
-            //Initialize Products
-//            products1.add(new ModelProduct("Sandwich", "Rio De Janeiro", "@drawable/sandwich","2$"));
-//            products1.add(new ModelProduct("Hot dog", "Hot Evening", "@drawable/sandwich", "2$ 50c"));
-//            products1.add(new ModelProduct("Sanwich", "Reptile", "@drawable/sandwich", "5$"));
-
 
             Bundle arg = getArguments();
             int pageNum = arg.getInt(ARG_OBJECT);
@@ -257,10 +215,6 @@ public class MainActivity extends AppCompatActivity
             mRecyclerView.setAdapter(mAdapter);
 
             return rootView;
-//            TextView textView = new TextView(getActivity());
-//            textView.setText("Hello from fragment" + pageNum);
-//            textView.setGravity(Gravity.CENTER);
-//            return textView;
         }
     }
 }
